1d67d6b63d82a2b308cf5d88939bfde2de4901cf
Jesus Castro <jesus.rcc59@gmail.com>
2024-04-19 18:24:43 -0600

    Ya funciona el registro

--- Registro.cs
+++ Registro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,35 @@ namespace RDGweb
         public FormRegistro()
         {
             InitializeComponent();
+        }
+
+        private void BtnGuardarDatos_Click(object sender, EventArgs e)
+        {
+            MySqlConnection con = new MySqlConnection("Server = localhost; Database = Guarderia; User Id = root; Password =  ");
+            try
+            {
+                con.Open();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error" + ex.ToString());
+                throw;
+            }
+            string sql = "insert into usuarios (correo, contrasena) values ('" + TbxRegistroUser.Text + "' , '" + TbxRegistroPassword.Text + "')";
+            MySqlCommand cmd = new MySqlCommand(sql, con);
+            try
+            {
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Usuario Registrado.");
+                this.Hide();
+                Form FormLogin = new FormLogin();
+                FormLogin.Show();
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error" + ex.ToString());
+            }
+
         }
     }
 }
