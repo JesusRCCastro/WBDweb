1142f599dde7a27cdfdae228896ce3f97cd9adc3
aram <aramlol412@outlook.com>
2024-04-28 01:14:17 -0600

    form de lista de trabajadores funcionando

--- Trabajadores.Designer.cs
+++ Trabajadores.Designer.cs
@@ -29,56 +29,78 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.CbxNombre = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.TextBoxCelularCliente = new System.Windows.Forms.TextBox();
-            this.TextBoxDirrecionCliente = new System.Windows.Forms.TextBox();
-            this.TextBoxEdadCliente = new System.Windows.Forms.TextBox();
+            this.TbxTelefonoTrabajador = new System.Windows.Forms.TextBox();
+            this.TbxCorreoTrabajador = new System.Windows.Forms.TextBox();
+            this.TbxRolTrabajador = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.TextBoxNombreCliente = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.TbxIDTrabajador = new System.Windows.Forms.TextBox();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TbxNombreTrabajador = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.CbxNombre);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(476, 110);
+            this.panel1.Size = new System.Drawing.Size(373, 81);
             this.panel1.TabIndex = 0;
             // 
-            // comboBox1
+            // label6
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(307, 43);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 0;
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(236, 20);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Nombre";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(72, 43);
+            this.label1.Location = new System.Drawing.Point(54, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 36);
+            this.label1.Size = new System.Drawing.Size(158, 29);
             this.label1.TabIndex = 1;
             this.label1.Text = "Trabajadores";
+            // 
+            // CbxNombre
+            // 
+            this.CbxNombre.FormattingEnabled = true;
+            this.CbxNombre.Location = new System.Drawing.Point(230, 35);
+            this.CbxNombre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.CbxNombre.Name = "CbxNombre";
+            this.CbxNombre.Size = new System.Drawing.Size(120, 21);
+            this.CbxNombre.TabIndex = 0;
+            this.CbxNombre.SelectedIndexChanged += new System.EventHandler(this.CbxNombre_SelectedIndexChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Honeydew;
-            this.label5.Location = new System.Drawing.Point(265, 201);
+            this.label5.Location = new System.Drawing.Point(22, 153);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 16);
+            this.label5.Size = new System.Drawing.Size(39, 13);
             this.label5.TabIndex = 20;
             this.label5.Text = "Celular";
             // 
@@ -86,9 +108,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(44, 141);
+            this.label4.Location = new System.Drawing.Point(21, 95);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(20, 16);
+            this.label4.Size = new System.Drawing.Size(18, 13);
             this.label4.TabIndex = 19;
             this.label4.Text = "ID";
             // 
@@ -96,77 +119,132 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Honeydew;
-            this.label3.Location = new System.Drawing.Point(44, 201);
+            this.label3.Location = new System.Drawing.Point(18, 215);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 16);
+            this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 18;
             this.label3.Text = "Correo";
             // 
-            // TextBoxCelularCliente
+            // TbxTelefonoTrabajador
             // 
-            this.TextBoxCelularCliente.Location = new System.Drawing.Point(268, 220);
-            this.TextBoxCelularCliente.Name = "TextBoxCelularCliente";
-            this.TextBoxCelularCliente.Size = new System.Drawing.Size(125, 22);
-            this.TextBoxCelularCliente.TabIndex = 17;
+            this.TbxTelefonoTrabajador.Location = new System.Drawing.Point(24, 169);
+            this.TbxTelefonoTrabajador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TbxTelefonoTrabajador.Name = "TbxTelefonoTrabajador";
+            this.TbxTelefonoTrabajador.Size = new System.Drawing.Size(95, 20);
+            this.TbxTelefonoTrabajador.TabIndex = 17;
             // 
-            // TextBoxDirrecionCliente
+            // TbxCorreoTrabajador
             // 
-            this.TextBoxDirrecionCliente.Location = new System.Drawing.Point(44, 220);
-            this.TextBoxDirrecionCliente.Name = "TextBoxDirrecionCliente";
-            this.TextBoxDirrecionCliente.Size = new System.Drawing.Size(154, 22);
-            this.TextBoxDirrecionCliente.TabIndex = 16;
+            this.TbxCorreoTrabajador.Location = new System.Drawing.Point(21, 230);
+            this.TbxCorreoTrabajador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TbxCorreoTrabajador.Name = "TbxCorreoTrabajador";
+            this.TbxCorreoTrabajador.Size = new System.Drawing.Size(171, 20);
+            this.TbxCorreoTrabajador.TabIndex = 16;
             // 
-            // TextBoxEdadCliente
+            // TbxRolTrabajador
             // 
-            this.TextBoxEdadCliente.Location = new System.Drawing.Point(268, 160);
-            this.TextBoxEdadCliente.Name = "TextBoxEdadCliente";
-            this.TextBoxEdadCliente.Size = new System.Drawing.Size(125, 22);
-            this.TextBoxEdadCliente.TabIndex = 15;
+            this.TbxRolTrabajador.Location = new System.Drawing.Point(210, 169);
+            this.TbxRolTrabajador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TbxRolTrabajador.Name = "TbxRolTrabajador";
+            this.TbxRolTrabajador.Size = new System.Drawing.Size(95, 20);
+            this.TbxRolTrabajador.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Honeydew;
-            this.label2.Location = new System.Drawing.Point(265, 141);
+            this.label2.Location = new System.Drawing.Point(208, 154);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.Size = new System.Drawing.Size(23, 13);
             this.label2.TabIndex = 14;
             this.label2.Text = "Rol";
             // 
-            // TextBoxNombreCliente
+            // TbxIDTrabajador
             // 
-            this.TextBoxNombreCliente.Location = new System.Drawing.Point(41, 160);
-            this.TextBoxNombreCliente.Name = "TextBoxNombreCliente";
-            this.TextBoxNombreCliente.Size = new System.Drawing.Size(67, 22);
-            this.TextBoxNombreCliente.TabIndex = 13;
+            this.TbxIDTrabajador.Location = new System.Drawing.Point(21, 110);
+            this.TbxIDTrabajador.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TbxIDTrabajador.Name = "TbxIDTrabajador";
+            this.TbxIDTrabajador.Size = new System.Drawing.Size(51, 20);
+            this.TbxIDTrabajador.TabIndex = 13;
             // 
-            // label6
+            // BtnAgregar
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(314, 24);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(56, 16);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Nombre";
+            this.BtnAgregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.BtnAgregar.Location = new System.Drawing.Point(14, 3);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(75, 34);
+            this.BtnAgregar.TabIndex = 21;
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.UseVisualStyleBackColor = false;
+            this.BtnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.BtnEliminar.Location = new System.Drawing.Point(286, 3);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(75, 34);
+            this.BtnEliminar.TabIndex = 22;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = false;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Info;
+            this.panel2.Controls.Add(this.BtnAgregar);
+            this.panel2.Controls.Add(this.BtnEliminar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 264);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(373, 40);
+            this.panel2.TabIndex = 23;
+            // 
+            // TbxNombreTrabajador
+            // 
+            this.TbxNombreTrabajador.Location = new System.Drawing.Point(209, 110);
+            this.TbxNombreTrabajador.Margin = new System.Windows.Forms.Padding(2);
+            this.TbxNombreTrabajador.Name = "TbxNombreTrabajador";
+            this.TbxNombreTrabajador.Size = new System.Drawing.Size(151, 20);
+            this.TbxNombreTrabajador.TabIndex = 25;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Honeydew;
+            this.label7.Location = new System.Drawing.Point(207, 95);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Nombre";
             // 
             // Trabajadores
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(476, 323);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(373, 304);
+            this.Controls.Add(this.TbxNombreTrabajador);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.TextBoxCelularCliente);
-            this.Controls.Add(this.TextBoxDirrecionCliente);
-            this.Controls.Add(this.TextBoxEdadCliente);
+            this.Controls.Add(this.TbxTelefonoTrabajador);
+            this.Controls.Add(this.TbxCorreoTrabajador);
+            this.Controls.Add(this.TbxRolTrabajador);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.TextBoxNombreCliente);
+            this.Controls.Add(this.TbxIDTrabajador);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Trabajadores";
             this.Text = "Trabajadores";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,15 +254,20 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox CbxNombre;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox TextBoxCelularCliente;
-        private System.Windows.Forms.TextBox TextBoxDirrecionCliente;
-        private System.Windows.Forms.TextBox TextBoxEdadCliente;
+        private System.Windows.Forms.TextBox TbxTelefonoTrabajador;
+        private System.Windows.Forms.TextBox TbxCorreoTrabajador;
+        private System.Windows.Forms.TextBox TbxRolTrabajador;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox TextBoxNombreCliente;
+        private System.Windows.Forms.TextBox TbxIDTrabajador;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox TbxNombreTrabajador;
+        private System.Windows.Forms.Label label7;
     }
 }
--- Trabajadores.cs
+++ Trabajadores.cs
@@ -7,14 +7,160 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace RDGweb
 {
     public partial class Trabajadores : Form
     {
+        private MySqlConnection con;
+        private string connectionString = "server=localhost;Database=Guarderia;Uid=root;Password=";
         public Trabajadores()
         {
             InitializeComponent();
+            con = new MySqlConnection(connectionString);
+            CargarTrabajadores();
         }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            string Nombre = TbxNombreTrabajador.Text;
+            string Roles = TbxRolTrabajador.Text;
+            string Correo = TbxCorreoTrabajador.Text;
+            string Telefono = TbxTelefonoTrabajador.Text;
+
+            if(!string.IsNullOrWhiteSpace(Nombre) && !string.IsNullOrWhiteSpace(Roles) && !string.IsNullOrWhiteSpace(Correo) && !string.IsNullOrWhiteSpace(Telefono))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "INSERT INTO `personal de la guarderia` (Nombre, Roles, Correo, telefono) VALUES (@nombre, @roles, @correo, @telefono)";
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@nombre", Nombre);
+                    cmd.Parameters.AddWithValue("@roles", Roles);
+                    cmd.Parameters.AddWithValue("@correo", Correo);
+                    cmd.Parameters.AddWithValue("@telefono", Telefono);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Trabajador agregado correctamente.");
+                    CargarTrabajadores();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error al agregar el trabajador: " + ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, completa todos los campos.");
+            }
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            string nombreABuscar = CbxNombre.SelectedItem.ToString();
+            if (!string.IsNullOrWhiteSpace(nombreABuscar))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "DELETE FROM `personal de la guarderia` WHERE Nombre = @nombre";
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@nombre", nombreABuscar);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Trabajador eliminado correctamente.");
+                    CargarTrabajadores();
+                    LimpiarTextBox();
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error al eliminar el trabajador: " + ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Por favor, selecciona un nombre para eliminar.");
+            }
+        }
+
+        private void CargarTrabajadores()
+        {
+            try
+            {
+                con.Open();
+                string query = "SELECT Nombre FROM `personal de la guarderia`";
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                CbxNombre.Items.Clear();
+                while (reader.Read())
+                {
+                    CbxNombre.Items.Add(reader["Nombre"].ToString());
+                }
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show("Error al cargar los trabajadores: " + ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void CbxNombre_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string nombreABuscar = CbxNombre.SelectedItem.ToString();
+            if (!string.IsNullOrWhiteSpace(nombreABuscar))
+            {
+                try
+                {
+                    con.Open();
+                    string query = "SELECT * FROM `personal de la guarderia` WHERE Nombre = @nombre";
+                    MySqlCommand cmd = new MySqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@nombre", nombreABuscar);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        TbxIDTrabajador.Text = reader["idPersonal"].ToString();
+                        TbxRolTrabajador.Text = reader["Roles"].ToString();
+                        TbxCorreoTrabajador.Text = reader["Correo"].ToString();
+                        TbxNombreTrabajador.Text = reader["Nombre"].ToString();
+                        TbxTelefonoTrabajador.Text = reader["telefono"].ToString();
+                    }
+                    else
+                    {
+                        LimpiarTextBox();
+                    }
+                }
+                catch (MySqlException ex)
+                {
+                    MessageBox.Show("Error al buscar el trabajador: " + ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+
+            }
+            
+                
+        }
+
+        private void LimpiarTextBox()
+        {
+                TbxIDTrabajador.Text = "";
+                TbxRolTrabajador.Text = "";
+                TbxCorreoTrabajador.Text = "";
+                TbxNombreTrabajador.Text = "";
+                TbxTelefonoTrabajador.Text = "";
+   
+        }             
     }
 }
