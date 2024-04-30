61b2df6c680de5ba31b9494f545c553cb4a560ec
Jesus Castro <jesus.rcc59@gmail.com>
2024-04-30 02:52:28 -0600

    Nueva asistencia y mejor diseño de asistencia

--- Asistencia.Designer.cs
+++ Asistencia.Designer.cs
@@ -28,31 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.TbxIdNiño = new System.Windows.Forms.TextBox();
             this.BtnTomarAsistencia = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.BtnBuscarNiño = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.BtnFalta = new System.Windows.Forms.Button();
             this.DgvAsistencia = new System.Windows.Forms.DataGridView();
-            this.ColNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.ColAsistencia = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.DgvAsistencia)).BeginInit();
             this.SuspendLayout();
             // 
-            // TbxIdNiño
-            // 
-            this.TbxIdNiño.Location = new System.Drawing.Point(149, 51);
-            this.TbxIdNiño.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.TbxIdNiño.Multiline = true;
-            this.TbxIdNiño.Name = "TbxIdNiño";
-            this.TbxIdNiño.Size = new System.Drawing.Size(135, 27);
-            this.TbxIdNiño.TabIndex = 0;
-            // 
             // BtnTomarAsistencia
             // 
-            this.BtnTomarAsistencia.Location = new System.Drawing.Point(403, 113);
+            this.BtnTomarAsistencia.Location = new System.Drawing.Point(435, 112);
             this.BtnTomarAsistencia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnTomarAsistencia.Name = "BtnTomarAsistencia";
             this.BtnTomarAsistencia.Size = new System.Drawing.Size(160, 38);
@@ -63,41 +50,28 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.panel1.Controls.Add(this.BtnBuscarNiño);
+            this.panel1.BackColor = System.Drawing.Color.SteelBlue;
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.TbxIdNiño);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(563, 100);
+            this.panel1.Size = new System.Drawing.Size(724, 93);
             this.panel1.TabIndex = 2;
-            // 
-            // BtnBuscarNiño
-            // 
-            this.BtnBuscarNiño.Location = new System.Drawing.Point(315, 45);
-            this.BtnBuscarNiño.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.BtnBuscarNiño.Name = "BtnBuscarNiño";
-            this.BtnBuscarNiño.Size = new System.Drawing.Size(123, 34);
-            this.BtnBuscarNiño.TabIndex = 6;
-            this.BtnBuscarNiño.Text = "Buscar Niño";
-            this.BtnBuscarNiño.UseVisualStyleBackColor = true;
-            this.BtnBuscarNiño.Click += new System.EventHandler(this.BtnBuscarNiño_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(127, 9);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(240, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 22);
+            this.label1.Size = new System.Drawing.Size(278, 32);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Ingrese el ID del Niño";
+            this.label1.Text = "Marque la Asistencia";
             // 
             // BtnFalta
             // 
-            this.BtnFalta.Location = new System.Drawing.Point(403, 263);
+            this.BtnFalta.Location = new System.Drawing.Point(435, 187);
             this.BtnFalta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BtnFalta.Name = "BtnFalta";
             this.BtnFalta.Size = new System.Drawing.Size(160, 38);
@@ -108,40 +82,22 @@
             // 
             // DgvAsistencia
             // 
-            this.DgvAsistencia.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.DgvAsistencia.BackgroundColor = System.Drawing.Color.SteelBlue;
             this.DgvAsistencia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DgvAsistencia.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.ColNombre,
-            this.ColAsistencia});
-            this.DgvAsistencia.Dock = System.Windows.Forms.DockStyle.Left;
-            this.DgvAsistencia.Location = new System.Drawing.Point(0, 100);
+            this.DgvAsistencia.Location = new System.Drawing.Point(205, 97);
             this.DgvAsistencia.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DgvAsistencia.Name = "DgvAsistencia";
             this.DgvAsistencia.RowHeadersWidth = 51;
             this.DgvAsistencia.RowTemplate.Height = 24;
-            this.DgvAsistencia.Size = new System.Drawing.Size(402, 243);
+            this.DgvAsistencia.Size = new System.Drawing.Size(206, 349);
             this.DgvAsistencia.TabIndex = 4;
-            // 
-            // ColNombre
-            // 
-            this.ColNombre.HeaderText = "Nombre del NIño";
-            this.ColNombre.MinimumWidth = 6;
-            this.ColNombre.Name = "ColNombre";
-            this.ColNombre.Width = 125;
-            // 
-            // ColAsistencia
-            // 
-            this.ColAsistencia.HeaderText = "Asistencia";
-            this.ColAsistencia.MinimumWidth = 6;
-            this.ColAsistencia.Name = "ColAsistencia";
-            this.ColAsistencia.Width = 125;
             // 
             // Asistencia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(563, 343);
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(724, 457);
             this.Controls.Add(this.DgvAsistencia);
             this.Controls.Add(this.BtnFalta);
             this.Controls.Add(this.panel1);
@@ -159,15 +115,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox TbxIdNiño;
         private System.Windows.Forms.Button BtnTomarAsistencia;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button BtnFalta;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView DgvAsistencia;
-        private System.Windows.Forms.Button BtnBuscarNiño;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColNombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ColAsistencia;
     }
 }
--- Asistencia.cs
+++ Asistencia.cs
@@ -1,12 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RDGweb
@@ -14,60 +8,31 @@ namespace RDGweb
     public partial class Asistencia : Form
     {
         MySqlConnection conexion;
-        MySqlCommand comando;
+
         public Asistencia()
         {
             InitializeComponent();
             // Inicializar la conexión a la base de datos
             string cadenaConexion = "server=localhost;user=root;password=;database=guarderia;";
             conexion = new MySqlConnection(cadenaConexion);
-
+            LlenarGridConNiños();
         }
 
-        private void BtnBuscarNiño_Click(object sender, EventArgs e)
+        private void LlenarGridConNiños()
         {
-            // Verificamos si se ingresó un ID de estudiante válido
-            if (string.IsNullOrWhiteSpace(TbxIdNiño.Text))
-            {
-                MessageBox.Show("Por favor, ingrese el ID del estudiante.");
-                return;
-            }
-
-            // Verificamos si el ID del estudiante es un número válido
-            if (!int.TryParse(TbxIdNiño.Text, out int idEstudiante))
-            {
-                MessageBox.Show("El ID del estudiante debe ser un número entero válido.");
-                return;
-            }
-
-            // Obtener el ID del niño desde el TextBox
-            int id = Convert.ToInt32(TbxIdNiño.Text);
-
-            // Consulta para obtener la información del niño
-            string consulta = "SELECT Nombre FROM niños WHERE idNiño = @id";
-
             try
             {
                 conexion.Open();
-                comando = new MySqlCommand(consulta, conexion);
-                comando.Parameters.AddWithValue("@id", id);
-
-                using (MySqlDataReader reader = comando.ExecuteReader())
-                {
-                    if (reader.Read())
-                    {
-                        // Mostrar el nombre del niño en la primera columna del DataGridView
-                        DgvAsistencia.Rows.Add(reader["nombre"].ToString(), "", "");
-                    }
-                    else
-                    {
-                        MessageBox.Show("No se encontró ningún niño con ese ID.");
-                    }
-                }
+                string query = "SELECT Nombre FROM niños";
+                MySqlCommand comando = new MySqlCommand(query, conexion);
+                MySqlDataAdapter adaptador = new MySqlDataAdapter(comando);
+                DataTable dt = new DataTable();
+                adaptador.Fill(dt);
+                DgvAsistencia.DataSource = dt;
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Error al buscar el niño: " + ex.Message);
+                MessageBox.Show("Error al cargar los datos: " + ex.Message);
             }
             finally
             {
@@ -77,94 +42,46 @@ namespace RDGweb
 
         private void BtnTomarAsistencia_Click(object sender, EventArgs e)
         {
-            // Verificamos si se ingresó un ID de estudiante válido
-            if (string.IsNullOrWhiteSpace(TbxIdNiño.Text))
+            // Verificamos si se ha seleccionado una fila en el DataGridView
+            if (DgvAsistencia.SelectedRows.Count > 0)
             {
-                MessageBox.Show("Por favor, ingrese el ID del estudiante.");
-                return;
-            }
+                // Obtener el índice de la fila seleccionada
+                int indiceFila = DgvAsistencia.SelectedRows[0].Index;
 
-            // Verificamos si el ID del estudiante es un número válido
-            if (!int.TryParse(TbxIdNiño.Text, out int idEstudiante))
+                // Obtener el nombre del niño de la primera columna del DataGridView
+                string nombreNiño = DgvAsistencia.Rows[indiceFila].Cells[0].Value.ToString();
+
+                // Mostrar un mensaje indicando que se ha tomado la asistencia para el niño seleccionado
+                MessageBox.Show("Se ha tomado la asistencia para " + nombreNiño);
+
+                // No es necesario marcar asistencia en el DataGridView, ya que se sigue mostrando el nombre del niño
+            }
+            else
             {
-                MessageBox.Show("El ID del estudiante debe ser un número entero válido.");
-                return;
+                MessageBox.Show("Por favor, seleccione una fila para marcar la asistencia.");
             }
-
-            // Obtener el índice de la fila seleccionada
-            int indiceFila = DgvAsistencia.CurrentRow.Index;
-
-            // Obtener el ID del niño
-            int id = Convert.ToInt32(TbxIdNiño.Text);
-
-            // Marcar asistencia en la segunda columna del DataGridView
-            DgvAsistencia.Rows[indiceFila].Cells[1].Value = "Asistencia";
-
-            // Guardar en la base de datos la asistencia del niño
-            string consulta = "UPDATE asistencia SET Presencia = 1 WHERE idNiño = @id";
-
-            try
-            {
-                conexion.Open();
-                comando = new MySqlCommand(consulta, conexion);
-                comando.Parameters.AddWithValue("@id", id);
-                comando.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error al marcar la asistencia: " + ex.Message);
-            }
-            finally
-            {
-                conexion.Close();
-            }
-
         }
 
         private void BtnFalta_Click(object sender, EventArgs e)
         {
-            // Verificamos si se ingresó un ID de estudiante válido
-            if (string.IsNullOrWhiteSpace(TbxIdNiño.Text))
+            // Verificamos si se ha seleccionado una fila en el DataGridView
+            if (DgvAsistencia.SelectedRows.Count > 0)
             {
-                MessageBox.Show("Por favor, ingrese el ID del estudiante.");
-                return;
-            }
+                // Obtener el índice de la fila seleccionada
+                int indiceFila = DgvAsistencia.SelectedRows[0].Index;
 
-            // Verificamos si el ID del estudiante es un número válido
-            if (!int.TryParse(TbxIdNiño.Text, out int idEstudiante))
+                // Obtener el nombre del niño de la primera columna del DataGridView
+                string nombreNiño = DgvAsistencia.Rows[indiceFila].Cells[0].Value.ToString();
+
+                // Mostrar un mensaje indicando que se ha marcado falta para el niño seleccionado
+                MessageBox.Show("Se ha marcado falta para " + nombreNiño);
+
+                // No es necesario marcar falta en el DataGridView, ya que se sigue mostrando el nombre del niño
+            }
+            else
             {
-                MessageBox.Show("El ID del estudiante debe ser un número entero válido.");
-                return;
+                MessageBox.Show("Por favor, seleccione una fila para marcar la falta.");
             }
-
-            // Obtener el índice de la fila seleccionada
-            int indiceFila = DgvAsistencia.CurrentRow.Index;
-
-            // Obtener el ID del niño
-            int id = Convert.ToInt32(TbxIdNiño.Text);
-
-            // Marcar falta en la tercera columna del DataGridView
-            DgvAsistencia.Rows[indiceFila].Cells[1].Value = "Falta";
-
-            // Guardar en la base de datos la falta del niño
-            string consulta = "UPDATE asistencia SET Ausente = 1 WHERE idNiño = @id";
-
-            try
-            {
-                conexion.Open();
-                comando = new MySqlCommand(consulta, conexion);
-                comando.Parameters.AddWithValue("@id", id);
-                comando.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("Error al marcar la falta: " + ex.Message);
-            }
-            finally
-            {
-                conexion.Close();
-            }
-
         }
 
         private void Asistencia_FormClosing(object sender, FormClosingEventArgs e)
